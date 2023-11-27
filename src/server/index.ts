4e04e4fc4a1f6e3597aaef66f19525982c668f97
Hugo Veiga <hugoveiga10@gmail.com>
2023-11-27 23:29:27 +0100

    Add PingController test suite

--- src/server/index.ts
+++ src/server/index.ts
@@ -4,9 +4,11 @@ import cors from "cors";
 import { app } from "./app.js";
 import { corsOptions } from "./utils/cors.js";
 import { generalError } from "./middlewares/errors/errorsMiddleware.js";
+import pingRouter from "../features/ping/router/pingRouter.js";
 
 app.use(morgan("dev"));
 app.use(cors(corsOptions));
 app.use(express.json());
 
+app.get("/", pingRouter);
 app.use(generalError);
