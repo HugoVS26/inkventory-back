214d1b45b664b9cb661ea4730846dc65a92086ca
Hugo Veiga <hugoveiga10@gmail.com>
2023-11-27 22:56:29 +0100

    Add generalError to errorsMiddleware

--- src/server/index.ts
+++ src/server/index.ts
@@ -3,7 +3,10 @@ import express from "express";
 import cors from "cors";
 import { app } from "./app.js";
 import { corsOptions } from "./utils/cors.js";
+import { generalError } from "./middlewares/errors/errorsMiddleware.js";
 
 app.use(morgan("dev"));
 app.use(cors(corsOptions));
 app.use(express.json());
+
+app.use(generalError);
