d90141ba366fdf0509bd3730f3aa07445b60449a
Hugo Veiga <hugoveiga10@gmail.com>
2023-12-06 14:51:39 +0100

    Add deleteTattoo to TattoosController

--- src/features/tattoos/controller/TattoosController.ts
+++ src/features/tattoos/controller/TattoosController.ts
@@ -1,4 +1,4 @@
-import { type Request, type Response } from "express";
+import { type NextFunction, type Request, type Response } from "express";
 import { type TattoosRepository } from "../repository/types";
 
 class TattoosController {
@@ -7,6 +7,17 @@ class TattoosController {
   getTattoos = async (_req: Request, res: Response) => {
     const tattoos = await this.tattoosRepository.getTattoos();
     res.status(200).json({ tattoos });
+  };
+
+  deleteTattoo = async (req: Request, res: Response, next: NextFunction) => {
+    const { id } = req.params;
+    try {
+      await this.tattoosRepository.deleteTattoo(id);
+
+      res.status(200).json({ message: "The tattoo has been deleted" });
+    } catch (error) {
+      next(error);
+    }
   };
 }
 
