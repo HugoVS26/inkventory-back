f8c69bcb1c712a0d6f60f720d0d09826b3660e7c
Hugo Veiga <hugoveiga10@gmail.com>
2023-12-08 10:05:20 +0100

    Add addTattoo to TattoosMongooseRepository

--- src/features/tattoos/repository/TattoosMongooseRepository.ts
+++ src/features/tattoos/repository/TattoosMongooseRepository.ts
@@ -1,6 +1,6 @@
 import CustomError from "../../../server/CustomError/CustomError.js";
 import Tattoos from "../model/Tattoos.js";
-import { type TattooStructure } from "../types";
+import { type TattooStructureWithoutId, type TattooStructure } from "../types";
 import { type TattoosRepository } from "./types";
 
 class TattoosMongooseRepository implements TattoosRepository {
@@ -15,6 +15,18 @@ class TattoosMongooseRepository implements TattoosRepository {
       await Tattoos.findByIdAndDelete(id);
     } catch (error) {
       throw new CustomError("Error deleting the tattoo", 400);
+    }
+  }
+
+  public async addTattoo(
+    tattoo: TattooStructureWithoutId,
+  ): Promise<TattooStructure> {
+    try {
+      const newTattoo = await Tattoos.create(tattoo);
+
+      return newTattoo;
+    } catch (error) {
+      throw new CustomError("Error creating the new tattoo", 400);
     }
   }
 }
